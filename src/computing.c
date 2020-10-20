2746d073ec987af2fce2331a831c75b5a1887514
Vladimir <Vladimir@gmail.com>
2020-10-21 04:14:55 +0700

    Add libruary words for past participle

--- src/computing.c
+++ src/computing.c
@@ -21,6 +21,25 @@ char past_simple[][16]
            "tore",    "told",   "thought", "threw", "understood", "woke",
            "wore",    "won",    "wrote"};
 
+char past_participle[][16]
+        = {"beaten",   "become", "begun",   "bent",   "bet",        "bitten",
+           "blown",    "broken", "brought", "built",  "bought",     "caught",
+           "chosen",   "come",   "cost",    "cut",    "dealt",      "dug",
+           "done",     "drawn",  "drunk",   "driven", "eaten",      "fallen",
+           "fed",      "felt",   "fought",  "found",  "flown",      "forgotten",
+           "forgiven", "frozen", "got",     "given",  "gone",       "grown",
+           "hung",     "had",    "heard",   "hidden", "hit",        "held",
+           "hurt",     "kept",   "known",   "laid",   "led",        "left",
+           "lent",     "let",    "lain",    "lit",    "lost",       "made",
+           "meant",    "met",    "paid",    "put",    "read",       "ridden",
+           "rung",     "risen",  "run",     "said",   "seen",       "sought",
+           "sold",     "sent",   "set",     "shaken", "shone",      "shot",
+           "showed",   "shut",   "sung",    "sunk",   "sat",        "slept",
+           "spoken",   "spent",  "stood",   "stolen", "stuck",      "struck",
+           "sworn",    "swept",  "swum",    "swung",  "taken",      "taught",
+           "torn",     "told",   "thought", "thrown", "understood", "woken",
+           "worn",     "won",    "written"};
+
 char past_simple[][16] = {"beat"};
 
 int check_pp(char check[], int error_pp, int random)
